7e51cb3324236bd553afb9a5eb143e90613b9d2b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-13 21:15:57 -0600

    Add reunion test for existance and attributes

---
+++ test/reunion_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/reunion'
+
+class ReunionTest < Minitest::Test
+
+  def setup
+    @reunion = Reunion.new("1406 BE")
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_it_has_attributes
+    assert_equal "1406 BE", reunion.name
+    assert_equal [], reunion.activities
+  end
+
+end

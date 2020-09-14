a6cd0d33f74b13aa77055dc87693e45d78a46453
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-13 21:19:10 -0600

    Add test for adding activities

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,11 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
 
   def setup
     @reunion = Reunion.new("1406 BE")
+    @activity_1 = Activity.new("Brunch")
   end
 
   def test_it_exists
@@ -15,6 +17,11 @@ class ReunionTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "1406 BE", @reunion.name
     assert_equal [], @reunion.activities
+  end
+
+  def test_can_add_activies
+    @reunion.add_activity(@activity_1)
+    assert_equal [@activity_1], @reunion.activities
   end
 
 end

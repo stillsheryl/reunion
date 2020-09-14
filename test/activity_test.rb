a376574377730644e53502def3f3f1cbc4656894
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-13 21:03:37 -0600

    Add test to calculate cost

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -30,4 +30,15 @@ class ActivityTest < Minitest::Test
     assert_equal expected2, activity.participants
   end
 
+  def test_calculates_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal 20, activity.total_cost
+
+    activity.add_participant("Luther", 40)
+
+    assert_equal 60, activity.total_cost
+  end
+
 end
